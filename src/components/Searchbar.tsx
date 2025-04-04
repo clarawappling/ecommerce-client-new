1d1c405082525c51555930bb81c5bee0d0d67701
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-04-04 10:24:59 +0200

    connection to search engine works as it should and gives relevant search results as per user input

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,13 +1,35 @@
 import { FormEvent, useState } from "react"
 import { useNavigate } from "react-router";
+import { APIResponse, IItem } from "../models/IItem";
+import axios from "axios";
 
 export const Searchbar = () => {
     const [searchText, setSearchText] = useState<string>("");
+    const [items, setItems] = useState<IItem[] | null>(null);
+    const [error, setError] = useState<string>("");
     const navigate = useNavigate()
 
-    const handleSubmit = (e: FormEvent) => {
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        navigate("/search-results");
+        
+        try {
+            const response = await axios.get <APIResponse> ('https://www.googleapis.com/customsearch/v1', {
+                params: {
+                    q: searchText,
+                    key: 'AIzaSyAZF8aD_9Nv1Li6rkqb2GYqVEOhpYtUS2k',
+                    cx: 'b62c9d26f9e65496e'
+                }
+            });
+            console.log(response.data);
+
+
+        } catch {
+
+        }
+        
+        
+        navigate("/search-results"); 
+        // Och någon mer parameter att skicka ned med useparams
         setSearchText("");
 
     }
