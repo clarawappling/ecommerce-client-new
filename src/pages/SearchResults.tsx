954361ca13550f0a2bbc9c9a49de40afb04270fe
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-04-08 13:46:50 +0200

    updated to another source for items and manually put those new items in

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,9 +1,88 @@
+// import axios from "axios";
+// import { useEffect, useState } from "react"
+// import { APIResponse, IItem } from "../models/IItem";
+// import { useNavigate, useParams } from "react-router";
+// import { URLmapping } from "../constants/URLMapping";
+// import "../styles/SearchResults.css"
+
+// export const SearchResults = () => {
+//     const [filteredResults, setFilteredResults] = useState<IItem[] | null>(null)
+//     const [error, setError] = useState<string>("");
+//     const params = useParams();
+//     const navigate = useNavigate();
+    
+
+//    useEffect (() => {
+//     const showSearchResult = async () => {
+//         setError('')
+//         if (!params.searchText) return;
+//         const searchTerm = params.searchText;
+//         try {
+//             const response = await axios.get <APIResponse> ('https://www.googleapis.com/customsearch/v1', {
+//                 params: {
+//                     q: searchTerm,
+//                     key: 'AIzaSyAZF8aD_9Nv1Li6rkqb2GYqVEOhpYtUS2k',
+//                     cx: 'b62c9d26f9e65496e'
+//                 }
+//             });
+    
+//             if (searchTerm.length <= 2) {
+//                 throw new Error('A search query must consist of at least 3 characters')
+//               }
+//               if(response.data.items === undefined) {
+//                 throw new Error('No results matched your query')
+//               }
+
+//               const resultItems = response.data.items
+//               console.log(response.data)
+//               const filteredResult = resultItems.filter(item => 
+//                 URLmapping.find(urlMapping => urlMapping.URL === item.link )
+//               );
+//               setFilteredResults(filteredResult)
+
+//         } catch(error: unknown) {
+        
+//             if(error instanceof Error) {
+//                 setError(error.message)
+//                 console.log(error);
+//               } }
+//        }
+//     showSearchResult()
+//     }, [params.searchText])
+ 
+
+//     const handleClick = (itemLink: string) => {
+//         const specificProduct = URLmapping.find(urlmapping => urlmapping.URL === itemLink)
+//         if(specificProduct)
+//         navigate("/product/" + specificProduct.id)
+//     }
+   
+//     if(error) return <p>{error}</p>
+//     if(filteredResults?.length === 0) return <p>Din sökning är för generell, försök skriva mer specifika sökord och pröva igen!</p>
+
+//     return (
+//         <>
+//         <h1>Sökresultat</h1>
+//         <div className="result-container">
+//         {filteredResults && filteredResults.map((item) => (
+//                 <div key={item.link} className="search-result-item">
+//                 {item.pagemap.cse_thumbnail && 
+//                 <img src={item.pagemap.cse_thumbnail[0].src} onClick={() => handleClick(item.link)}/>}
+//                 <h3>{item.title}</h3>
+//                 <p>{item.snippet}</p>
+//                 </div>
+//         ))}
+//         </div>
+//         </>
+//     )
+// }
+
 import axios from "axios";
 import { useEffect, useState } from "react"
 import { APIResponse, IItem } from "../models/IItem";
 import { useNavigate, useParams } from "react-router";
 import { URLmapping } from "../constants/URLMapping";
-
+import "../styles/SearchResults.css"
 
 export const SearchResults = () => {
     const [filteredResults, setFilteredResults] = useState<IItem[] | null>(null)
@@ -21,8 +100,8 @@ export const SearchResults = () => {
             const response = await axios.get <APIResponse> ('https://www.googleapis.com/customsearch/v1', {
                 params: {
                     q: searchTerm,
-                    key: 'AIzaSyAZF8aD_9Nv1Li6rkqb2GYqVEOhpYtUS2k',
-                    cx: 'b62c9d26f9e65496e'
+                    key: 'AIzaSyBjWwaodYmlbgOzhNeUhN6zF3js7EgqNaY',
+                    cx: 'e3331809a69db446d'
                 }
             });
     
@@ -34,6 +113,7 @@ export const SearchResults = () => {
               }
 
               const resultItems = response.data.items
+              console.log(response.data)
               const filteredResult = resultItems.filter(item => 
                 URLmapping.find(urlMapping => urlMapping.URL === item.link )
               );
@@ -57,9 +137,11 @@ export const SearchResults = () => {
     }
    
     if(error) return <p>{error}</p>
+    if(filteredResults?.length === 0) return <p>Din sökning är för generell, försök skriva mer specifika sökord och pröva igen!</p>
 
     return (
         <>
+        <h1>Sökresultat</h1>
         <div className="result-container">
         {filteredResults && filteredResults.map((item) => (
                 <div key={item.link} className="search-result-item">
