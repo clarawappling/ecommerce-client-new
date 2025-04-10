1dcf4839a92472befd4fa27a32f692541eac8e12
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-04-10 10:23:46 +0200

    new pic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import "../styles/Home.css"
+import bobhund from "../assets/bobhundoriginal.jpg"
 
 export const Home = () => {
     
     return (
         <>
         <div>
-            <h1>Jonas leksaksgömma</h1>
+            
             <div className="img-container">
-                <img src="https://img.freepik.com/premium-photo/positive-vibes-cute-teddy-bear-toy-softness-tenderness-playful-adult-cute-romantic-bearded-hipster-play-toy-yellow-background-valentines-day-gift-man-hug-soft-toy-make-surprise-concept_474717-117772.jpg"></img>
+                <img src={bobhund}/>
+                <h1>BOB HUND FOREVER - THE STORE</h1>
             </div>
         </div>
         
