af5e3a06a265b11d3fd20677b03d4e1f9ec57e6b
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-04-10 15:41:58 +0200

    text change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import "../styles/Footer.css"
 export const Footer = () => {
     return (
         <div>
-          <p>&copy; 2025 Juguetes Escondidos. All rights reserved.</p>
+          <p>&copy; 2025 Bob Hund Forever</p>
     <p>
         <a href="/">GDPR</a> | 
         <a href="/">Kontakt</a>
